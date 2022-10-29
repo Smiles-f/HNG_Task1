5e28461ba20e835e13a6d15ed06fca55791944d1
Smiles-f <102075210+Smiles-f@users.noreply.github.com>
2022-10-29 21:07:04 +0100

    Update views.py

--- task/views.py
+++ task/views.py
@@ -4,5 +4,5 @@ from django.http import JsonResponse
 # Create your views here.
 def intro(request):
     
-    return JsonResponse({'slackusername':'fish', 'backend':True, 'age': 25, 
+    return JsonResponse({'slackUsername':'fish', 'backend':True, 'age': 25, 
     'bio': 'i am a backend intern at HNG internship program'})
